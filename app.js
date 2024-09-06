892f3df6c206f6ab1689ab3ff41b3dd9b7e16ac1
Yashkumar patil <136915825+yashpatil209@users.noreply.github.com>
2024-09-07 00:17:00 +0530

    secound commit

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-require('dotenv').config();
-
 const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
-
 const app = express();
-const PORT = 8080;
-  
-// Connect to DB
-connectDB();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(methodOverride('_method'));
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI
-  }),
-  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
-}));
-
-app.use(express.static('public'));
-
-// Templating Engine
-app.use(expressLayout);
-app.set('layout', './layouts/main');
+const path = require('path');
+const mongoose = require('mongoose');
+const Blog = require("./models/blog");
+mongoose.connect("mongodb://riteshponty:VcCujxKVZPImncPV@ac-aztryrz-shard-00-00.kieyyok.mongodb.net:27017,ac-aztryrz-shard-00-01.kieyyok.mongodb.net:27017,ac-aztryrz-shard-00-02.kieyyok.mongodb.net:27017/?ssl=true&replicaSet=atlas-woodvy-shard-0&authSource=admin&retryWrites=true&w=majority").then(() => {
+    console.log('database connected');
+}).catch((err) => {
+    console.log(err);
+})
+app.get("/", (req, res) => {
+    res.redirect("/add-blog")
+})
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.urlencoded({ extended: true }));
+const port = 3000;
+app.get("/show-blogs",async (req, res) => {
+    const allBlogs = await Blog.find({});
+    console.log(allBlogs)
+    res.render('Show_blogs',{allBlogs})
+})
+app.get('/add-blog', (req, res) => {
+    res.render("add_item");
+})
+app.post('/add-blog', async(req, res) => {
+    console.log(req.body);
+    const { fname, lname, email, title, image1,image2,image3,image4, content } = req.body;
 
-
-app.locals.isActiveRoute = isActiveRoute; 
-
-
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
-
-app.listen(PORT, ()=> {
-  console.log(`App listening on port ${PORT}`);
-});
+    const isdatastored=await Blog.create({ name: fname + " " + lname, email: email, photo: [image1,image2,image3,image4], text: content, title: title });
+    if (!isdatastored) {
+        console.log('Err');
+    }
+    console.log('Data stored Success');
+    res.redirect("/show-blogs")
+})
+app.get("/read-blog/:id",async(req, res) => {
+    const { id } = req.params;
+    const item = await Blog.findById(id);
+    console.log(item);
+    res.render('read_blogs',{item})
+})
+app.post("/delete-blog/:id",async (req, res) => {
+    const { id } = req.params;
+    const deleteitem=await Blog.findByIdAndDelete(id);
+    if (!deleteitem) {
+        console.log('Item not deleted');
+    }
+    console.log('Item deleted');
+    res.redirect("/show-blogs")
+})
+app.listen(port, () => { console.log('Server is running at port 3000') });
